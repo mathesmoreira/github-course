ab10c0b492c598745bc9e9318373dad1be66e064
Matheus P Moreira <matheus.paula.moreira@usp.br>
2020-06-02 10:27:23 -0300

    Add acao_bovespa.py

---
+++ acao_bovespa.py
@@ -0,0 +1,19 @@
+import numpy as np
+import pandas as pd
+from pandas_datareader import data as wb
+import matplotlib.pyplot as plt
+
+
+TickerA='ITSA4.SA'
+TickerB='FLRY3.SA'
+TickerC='LREN3.SA'
+prices=pd.DataFrame()
+tickers = [TickerA, TickerB, TickerC]
+
+for t in tickers:
+    prices[t]=wb.DataReader(t, data_source='yahoo', start='2010-1-1')['Adj Close']
+
+(prices/prices.iloc[0]*100).plot(figsize=(15,5))
+plt.ylabel('NORMALIZED PRICES')
+plt.xlabel('DATE')
+plt.show()
